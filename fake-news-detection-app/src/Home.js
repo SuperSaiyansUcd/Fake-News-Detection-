32e5c637cb6142ff9655e22060a311f9dc523772
soggyfox <44605305+soggyfox@users.noreply.github.com>
2023-06-15 20:54:35 +0100

    update equal comparator

--- fake-news-detection-app/src/Home.js
+++ fake-news-detection-app/src/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
 
     const navigate = useNavigate();
     const toResult = () => {
-        if (title != "") {
+        if (title !== "") {
             navigate('Result', { title, content } );
         }
         

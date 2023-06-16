79b28cb4897d9861971928785923a1efe2a6d2eb
Jianhao Feng <jianhao.feng@ucdconnect.ie>
2023-06-16 12:06:09 +0100

    add missing return to home button

--- fake-news-detection-app/src/Result.js
+++ fake-news-detection-app/src/Result.js
@@ -1,8 +1,16 @@
 import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Result() {
     const location = useLocation();
     const { title, content } = location.state;
+
+    const navigate = useNavigate();
+    const toHome = (e) => {
+        
+        navigate('/');   
+    };
 
     return (<>
         <div>
@@ -12,6 +20,7 @@ export default function Result() {
             <p>Title: {title}</p>
             <p>Content: {content}</p>
         </div>
+        <button onClick={ toHome }>Return to home page</button>
         </>
     );
 }

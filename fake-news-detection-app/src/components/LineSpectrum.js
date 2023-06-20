f4e78b6dacb44ba82da2a9145df8f3122fca31c2
soggyfox <44605305+soggyfox@users.noreply.github.com>
2023-06-20 16:55:47 +0100

    arrow fix

--- fake-news-detection-app/src/components/LineSpectrum.js
+++ fake-news-detection-app/src/components/LineSpectrum.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './LineSpectrum.css';
 
 const LineSpectrum = ({ value }) => {
-    const spectrumWidth = 100;
-    const arrowPosition = (value / 100) * spectrumWidth;
+  const spectrumWidth = 100;
+  const arrowPosition = (value / 100) * spectrumWidth;
 
-    return (
-        <div className="spectrum-container">
-            <div className="line-spectrum" />
-            <div className="arrow" style={{ left: `${arrowPosition}%` }} />
-        </div>
-    );
+  return (
+    <div className="spectrum-container">
+      <div className="line-spectrum" />
+      <div className="arrow" style={{ left: `${arrowPosition}%` }}>
+        <div className="arrow-triangle"></div>
+        <div className="arrow-rectangle"></div>
+        <div className="value">{value}%</div>
+      </div>
+    </div>
+  );
 };
 
 export default LineSpectrum;

ef880f949c83cba9614ecf71ef1ebbe64f77c655
soggyfox <44605305+soggyfox@users.noreply.github.com>
2023-06-20 17:10:31 +0100

    add labels

--- fake-news-detection-app/src/components/LineSpectrum.js
+++ fake-news-detection-app/src/components/LineSpectrum.js
@@ -13,6 +13,10 @@ const LineSpectrum = ({ value }) => {
         <div className="arrow-rectangle"></div>
         <div className="value">{value}%</div>
       </div>
+      <div className="end-labels">
+        <div className="end-label">False</div>
+        <div className="end-label">True</div>
+      </div>
     </div>
   );
 };

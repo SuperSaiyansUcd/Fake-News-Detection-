f807d54862862211c3a21b82d756c0b83f775148
Madhav Mohan <maddy39307@gmail.com>
2023-06-26 05:41:02 +0100

    Mvp_demo

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,49 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+from textblob import TextBlob
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS
 
-# members from front-end
+# Load the saved model
+model = load_model('model.h5')
+tokenizer = Tokenizer()
+max_seq_length = None  # Define the maximum sequence length used during training
+
+nltk.download('stopwords')
+nltk.download('punkt')
+stop_words = set(stopwords.words('english'))
+
+def preprocess_text(text):
+    # Convert text to lowercase
+    text = text.lower()
+
+    # Remove URLs
+    text = re.sub(r"http\S+|www\S+|https\S+", "", text, flags=re.MULTILINE)
+
+    # Remove special characters and numbers
+    text = re.sub(r"[^\w\s]", "", text)
+    text = re.sub(r"\d+", "", text)
+
+    # Tokenize the text
+    tokens = word_tokenize(text)
+
+    # Remove stop words
+    tokens = [token for token in tokens if token not in stop_words]
+
+    # Join the tokens back into a single string
+    preprocessed_text = " ".join(tokens)
+
+    return preprocessed_text
+
 @app.route('/api/submit', methods=['POST'])
 def submit_data():
     # Handle POST request 
@@ -12,17 +51,29 @@ def submit_data():
     content = request.json.get('content')
 
     # Process the form data
-    # TODO
+    preprocessed_content = preprocess_text(content)
 
-    # Return a response
+    # Perform sentiment analysis using TextBlob
+    sentiment_score = TextBlob(preprocessed_content).sentiment.polarity
+
+    # Tokenize and pad the text
+    text_sequence = tokenizer.texts_to_sequences([preprocessed_content])
+    text_sequence = pad_sequences(text_sequence, maxlen=max_seq_length)
+
+    # Make predictions
+    prediction = model.predict_classes(text_sequence)[0][0]
+    is_fake = bool(prediction)  # Convert prediction to a boolean value
+
+    # Return the prediction and sentiment analysis score
     response = {
         'message': 'Form data received :)',
         'title': title,
-        'content': content
+        'content': content,
+        'is_fake': is_fake,
+        'sentiment_score': sentiment_score
     }
     return jsonify(response), 200
 
-# members to frontend
 @app.route("/members")
 def members():
     return jsonify({"members": ["mem1", "mem2"]})

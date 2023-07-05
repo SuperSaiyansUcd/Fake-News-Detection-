04b56845291fbba479e3f9a755b496143c59c2af
soggyfox <44605305+soggyfox@users.noreply.github.com>
2023-07-05 13:55:45 +0100

    add manifest

--- fake-news-detection-app/src/App.js
+++ fake-news-detection-app/src/App.js
@@ -1,32 +1,20 @@
-import "./index.css"
+import React from "react";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./Home";
 import Result from "./Result";
-import React, {useState, useEffect } from "react";
+import ExtensionPopup from './ExtensionPopup';
+import "./index.css"
 
-  // backend data https://github.com/miguelgrinberg/react-flask-app
-export default function App() {
-
-  // const [data, setData] = useState([{}])
-
-  // useEffect(() => {
-  //   fetch("http://127.0.0.1:5000/members").then(
-  //     res => res.json()
-  //   ).then(
-  //     data => {
-  //       setData(data)
-  //       console.log(data, "hi")
-  //     }
-  //   )
-  // }, [])
-
+const App = () => {
   return (
-  <Router>
-    <Routes>       
-      <Route path="/" element={<Home/>} />   
-      <Route path="/result" element={<Result/>} />
-    </Routes>
-  </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+      <Route path="/popup" element={<ExtensionPopup />} /> {/* Add the route for the extension popup */}
+    </Router>
   );
-}
+};
 
+export default App;

4801a1034f6bb99616d007688613f6ffa417c58a
soggyfox <44605305+soggyfox@users.noreply.github.com>
2023-07-05 15:09:07 +0100

    revert popup adds

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,25 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Result from "./Result";
-import Popup from "./Popup";
 import "./index.css";
 
 const App = () => {
-  const location = useLocation();
-
-  // Check if the app is running as a Chrome extension
-  const isExtension = location.pathname.startsWith("/popup");
 
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/result" element={<Result />} />
-        {isExtension && <Route path="/popup" element={<Popup />} />}
+      <Routes>       
+        <Route path="/" element={<Home/>} />   
+        <Route path="/result" element={<Result/>} />
       </Routes>
     </Router>
-  );
+    );
 };
 
 export default App;

a64cc3853fe7505fb2f7c091c939862ef84df865
soggyfox <44605305+soggyfox@users.noreply.github.com>
2023-07-06 10:35:47 +0100

    remove raw axios

--- fake-news-detection-app/src/Home.js
+++ fake-news-detection-app/src/Home.js
@@ -1,7 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { RawAxiosRequestConfig } from 'axios';
 
 export default function Home() {
   const [title, setTitle] = useState('');

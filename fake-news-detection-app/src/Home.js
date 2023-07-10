8661b089a4aaf9b0a8ec21d230ae5c163e548cf4
Jianhao Feng <116632169+Martin19999@users.noreply.github.com>
2023-07-10 12:05:50 +0100

    Add error handling to Homepage
    sanitization is not needed for front-end. Reactjs has some kind of default sanitization. Back-end needs tho

--- fake-news-detection-app/src/Home.js
+++ fake-news-detection-app/src/Home.js
@@ -10,7 +10,16 @@ export default function Home() {
 
   const toResult = (e) => {
     e.preventDefault();
-    if (title !== '') {
+
+    if (content === null) {
+        alert('Content field cannot be empty!!');
+    } else if ((content !== null && content.trim().length === 0)) {
+        console.log(title)
+        console.log(title.trim())
+        console.log(content)
+        console.log(content.trim())
+        alert('Content field cannot be empty!!');
+    } else {
       // Send data using axios
       axios
         .post('http://127.0.0.1:5000/api/submit', { title, content })
@@ -67,17 +76,15 @@ export default function Home() {
                 <h1 onClick={scrollToLast}>Fake News Detector</h1>
             </section>  
             <section id="section2" className="section">
-                <form onSubmit={toResult}> {
-                /* Add input validation and onSubmit event handler to pass json to backend */
-                }
+                <form onSubmit={toResult}> {/* Add input validation and onSubmit event handler to pass json to backend */}
                     <div>
-                      <label htmlFor="inbox1">Title:</label>
-                      <input
-                        value={title || ""}
-                        type="text"
-                        onChange={e => setTitle(e.target.value)}
-                        className="input-box"
-                      />
+                        <label htmlFor="inbox1">Title:</label>
+                        <input
+                            value={title || ""}
+                            type="text"
+                            onChange={e => setTitle(e.target.value)}
+                            className="input-box"
+                        />
                     </div>
                     <div>
                         <label htmlFor="inbox2">Content:</label>
@@ -86,6 +93,7 @@ export default function Home() {
                             type="text"
                             onChange={e => setContent(e.target.value)}
                             className="input-box"
+                            placeholder="Cannot be empty" 
                         />
                     </div>
                     <div>

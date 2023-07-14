68228bd7eb51587df2b081f8e522b8376409afd8
Jianhao Feng <116632169+Martin19999@users.noreply.github.com>
2023-07-14 12:38:25 +0100

    Add contact and credits pages

--- fake-news-detection-app/src/App.js
+++ fake-news-detection-app/src/App.js
@@ -2,29 +2,20 @@ import "./index.css"
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./Home";
 import Result from "./Result";
+import Contact from "./Contact";
+import Credits from "./Credits";
 import React from "react";
 
   // backend data https://github.com/miguelgrinberg/react-flask-app
 export default function App() {
-
-  // const [data, setData] = useState([{}])
-
-  // useEffect(() => {
-  //   fetch("http://127.0.0.1:5000/members").then(
-  //     res => res.json()
-  //   ).then(
-  //     data => {
-  //       setData(data)
-  //       console.log(data, "hi")
-  //     }
-  //   )
-  // }, [])
 
   return (
   <Router>
     <Routes>       
       <Route path="/" element={<Home/>} />   
       <Route path="/result" element={<Result/>} />
+      <Route path="/contact" element={<Contact/>} />   
+      <Route path="/credits" element={<Credits/>} />
     </Routes>
   </Router>
   );

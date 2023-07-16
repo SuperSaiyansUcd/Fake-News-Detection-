9f7643b6821c57713b6ac5b556d4d41337f45143
soggyfox <44605305+soggyfox@users.noreply.github.com>
2023-07-16 10:58:40 +0100

    remove unused

--- fake-news-detection-app/src/Contact.js
+++ fake-news-detection-app/src/Contact.js
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 export default function Contact() {
     const navigate = useNavigate();

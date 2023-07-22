dffd82c217fd8d2d928691ad9997be0c09a39549
soggyfox <44605305+soggyfox@users.noreply.github.com>
2023-07-22 20:20:37 +0100

    revamp

--- fake-news-detection-app/src/Learn.js
+++ fake-news-detection-app/src/Learn.js
@@ -1,7 +1,8 @@
 import { useNavigate } from 'react-router-dom';
-import './Credit.css';
+import './Learn.css';
+import matrixImage from './Images/matrix.png';
 
-export default function Credits() {
+export default function Learn() {
     const navigate = useNavigate();
 
     const toHome = (e) => {
@@ -11,7 +12,7 @@ export default function Credits() {
 
     return (
         <>
-            <div className='creditsPage'>
+            <div className='learnsPage'>
                 <div className="dropdown">
                     <button>☰</button>
                     <div className="dropdown-content">
@@ -23,23 +24,36 @@ export default function Credits() {
                     </div>
                 </div>
 
-                <div className='part1'>
+                <ul>
                     <h2>Learn More</h2>
-                </div>
-                <ul className='part2'>
                     <h4>Model Types</h4>
-                <li>Long Short-Term Memory (LSTM): This is a type of recurrent neural network that solves the <a href="https://en.wikipedia.org/wiki/Vanishing_gradient_problem" target="_blank"><u>vanishing gradient problem</u></a></li>
-                    <li>An Ensemble Model is a machine learning model that combines the predicitions of multiple models to achieve better performance </li>
-                    <li>Our "Ensemble"(together) Model makes use of  </li>
-                </ul>
+                    <li>Long Short-Term Memory (LSTM): This is a type of recurrent neural network that solves the <a href="https://en.wikipedia.org/wiki/Vanishing_gradient_problem" target="_blank"><u>vanishing gradient problem</u> &rarr;</a></li>
+                    <br></br>
+                    <li>An Ensemble("together") Model is a machine learning model that combines the predicitions of multiple models to achieve better performance </li>
+                    <br></br>
+                    <li>Our <a href="https://en.wikipedia.org/wiki/Ensemble_learning" target="_blank">
+                        <u>Ensemble</u></a> Model combines
+                        <a href="https://www.ibm.com/topics/naive-bayes#:~:text=The%20Na%C3%AFve%20Bayes%20classifier%20is,a%20given%20class%20or%20category" target="_blank">
+                            <u>   Naive Bayes</u></a> and <a href="https://jmlr.csail.mit.edu/papers/volume7/crammer06a/crammer06a.pdf" target="_blank">
+                            <u> Passive Aggressive Classifier</u>
+                        </a> with an additional layer of a deep neural network.
+                        <br></br>
+                        For more on Neural Networks and Deep Learning, you can refer to
+                        <a href="https://news.mit.edu/2017/explained-neural-networks-deep-learning-0414" target="_blank"><u> this article</u></a>.</li>
 
-                <ul className='part2'>
+                    <br></br><br></br>
                     <h4>Parameter Types</h4>
-                    <li>Homepage picture 1 - Photo by Mika Baumeister on Unsplash</li>
-                </ul>
-                <div className='part3'>
+
+
+                    <ul>
+                        <img src={matrixImage} alt="Confusion Matrix" />
+                        <br></br>
+                        <a href="https://www.researchgate.net/figure/Calculation-of-Precision-Recall-and-Accuracy-in-the-confusion-matrix_fig3_336402347" target="_blank"><u>Matrix Link</u></a>
+
+
+                    </ul>
                     <button className='button' onClick={toHome}>Return to home page</button>
-                </div>
+                </ul>
             </div>
         </>
     );

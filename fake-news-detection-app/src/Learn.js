9e1d97dfee1c411bbaab288f6dfcfe42f8be9395
soggyfox <44605305+soggyfox@users.noreply.github.com>
2023-07-22 20:32:13 +0100

    security patch

--- fake-news-detection-app/src/Learn.js
+++ fake-news-detection-app/src/Learn.js
@@ -31,15 +31,21 @@ export default function Learn() {
                     <br></br>
                     <li>An Ensemble("together") Model is a machine learning model that combines the predicitions of multiple models to achieve better performance </li>
                     <br></br>
-                    <li>Our <a href="https://en.wikipedia.org/wiki/Ensemble_learning" target="_blank">
-                        <u>Ensemble</u></a> Model combines
-                        <a href="https://www.ibm.com/topics/naive-bayes#:~:text=The%20Na%C3%AFve%20Bayes%20classifier%20is,a%20given%20class%20or%20category" target="_blank">
-                            <u>   Naive Bayes</u></a> and <a href="https://jmlr.csail.mit.edu/papers/volume7/crammer06a/crammer06a.pdf" target="_blank">
+                    <li>Our
+                        <a href="https://en.wikipedia.org/wiki/Ensemble_learning"
+                            target="_blank" rel="noopener noreferrer">
+                            <u>Ensemble</u></a> Model combines
+                        <a href="https://www.ibm.com/topics/naive-bayes#:~:text=The%20Na%C3%AFve%20Bayes%20classifier%20is,a%20given%20class%20or%20category"
+                            target="_blank" rel="noopener noreferrer">
+                            <u>   Naive Bayes</u></a> and
+                        <a href="https://jmlr.csail.mit.edu/papers/volume7/crammer06a/crammer06a.pdf"
+                            target="_blank" rel="noopener noreferrer">
                             <u> Passive Aggressive Classifier</u>
                         </a> with an additional layer of a deep neural network.
                         <br></br>
                         For more on Neural Networks and Deep Learning, you can refer to
-                        <a href="https://news.mit.edu/2017/explained-neural-networks-deep-learning-0414" target="_blank"><u> this article</u></a>.</li>
+                        <a href="https://news.mit.edu/2017/explained-neural-networks-deep-learning-0414"
+                            target="_blank" rel="noopener noreferrer"><u> this article</u></a>.</li>
 
                     <br></br><br></br>
                     <h4>Parameter Types</h4>
@@ -48,7 +54,7 @@ export default function Learn() {
                     <ul>
                         <img src={matrixImage} alt="Confusion Matrix" />
                         <br></br>
-                        <a href="https://www.researchgate.net/figure/Calculation-of-Precision-Recall-and-Accuracy-in-the-confusion-matrix_fig3_336402347" target="_blank"><u>Matrix Link</u></a>
+                        <a href="https://www.researchgate.net/figure/Calculation-of-Precision-Recall-and-Accuracy-in-the-confusion-matrix_fig3_336402347" target="_blank" rel="noopener noreferrer"><u>Matrix Link</u></a>
 
 
                     </ul>

4156ac79442bf215ccd66bf5e6bd270979ab853d
soggyfox <singpurwalakevin@gmail.com>
2023-07-31 23:43:36 +0100

    jus learn

--- fake-news-detection-app/src/Learn.js
+++ fake-news-detection-app/src/Learn.js
@@ -24,28 +24,30 @@ export default function Learn() {
                     </div>
                 </div>
 
-                <ul>
+                <ul class="red-colon">
                     <h2>Learn More</h2>
                     <h4>Model Types</h4>
-                    <li>Long Short-Term Memory (LSTM): This is a type of recurrent neural network that solves the <a href="https://en.wikipedia.org/wiki/Vanishing_gradient_problem"
+                    <li  class="red-colon">Long Short-Term Memory (LSTM): This is a type of recurrent neural network that solves the <a href="https://en.wikipedia.org/wiki/Vanishing_gradient_problem"
                         target="_blank" rel="noopener noreferrer">
                         <u>vanishing gradient problem</u> &rarr;</a></li>
                     <br></br>
                     <li>An Ensemble("together") Model is a machine learning model that combines the predicitions of multiple models to achieve better performance </li>
                     <br></br>
-                    <li>Our Models work best on twitter tweets and are less reliable for other news sources. This is due to our training data sets</li>
-
-                    <br></br><br></br>
-                    <h4>Parameter Types</h4>
-
-
                     <ul>
-                        <img src={matrixImage} alt="Confusion Matrix" />
-                        <br></br>
-                        <a href="https://www.researchgate.net/figure/Calculation-of-Precision-Recall-and-Accuracy-in-the-confusion-matrix_fig3_336402347" target="_blank" rel="noopener noreferrer"><u>Matrix Link</u></a>
+                        
+                    {/* <li>An Ensemble Model is a machine learning model that combines the predictions of multiple models to achieve better performance.</li> */}
+                    {/* <br></br> */}
 
+                    <li>Our Models work best on Twitter tweets and are less reliable for other news sources. This is due to our training data sets.</li>
+                    <br></br>
 
+                    <h4>Sentiment Types</h4>
+
+                    <li>Sentiment in text is a measure of different feelings present in the content. A higher value for a given feeling indicates stronger presence</li>
+                    <li>  The Vader Score, Afinn Score, Pattern score and TextBlob Score are an overall sentiment indicator. They range from negative, neutral to positive.</li>
+                    <li> The Majority Voting result is the overall sentiment prediction obtained by combining all our measures of sentiment analysis</li>
                     </ul>
+
                     <button className='button' onClick={toHome}>Return to home page</button>
                 </ul>
             </div>

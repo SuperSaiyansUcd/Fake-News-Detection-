c912f2db7ea09ae0ac05a90feafc51b0f1bf7e71
AnjuKrishnaSree-Git <sree.anjukrishna99@gmail.com>
2023-07-31 14:15:43 +0530

    updating backgound.js and manifest file

--- chromium-extension-app/public/background.js
+++ chromium-extension-app/public/background.js
@@ -1,27 +1,24 @@
-//create context menu options.  the 'on click' command is no longer valid in manifest version 3
+// Create a context menu item
+    chrome.runtime.onInstalled.addListener(function() {
+        chrome.contextMenus.create({
+          id: "sendToApp",
+          title: "Send to Fake News Detection Website",
+          contexts: ["selection"]
+        });
+    });
 
-chrome.contextMenus.create({
-    title: "Send to Fake News Detection System",
-    id: "sendToApp",
-    contexts: ["selection"]
-  });
-  
+    // Listen for a context menu click event
+    chrome.contextMenus.onClicked.addListener(function(info, tab) {
+      // Check if the clicked item has a specific ID (you can customize this ID)
+      if (info.menuItemId === "sendToApp") {
+        // Get the selected text from the page
+        sendToApplication(info.selectionText);
+      }
+    });
 
-//This tells the context menu what function to run when the option is selected
-
-chrome.contextMenus.onClicked.addListener(newTab);
-
-//Setting up the function to open the new tab
-function newTab(info,tab)
-{
-  const { menuItemId } = info
-
-  if (menuItemId === 'sendToApp'){
-    chrome.tabs.create({
-      url: "https://supersaiyans.azurewebsites.net/#auto/fa/" + info.selectionText
-      
-    })}};
-
-    
-
-
+    // Function to send the highlighted text to the website
+    function sendToApplication(selectedText) {
+        
+        console.log("Selected text:", selectedText);
+        chrome.tabs.create({ url: "http://localhost:3000/?text=" + encodeURIComponent(selectedText) });
+    }

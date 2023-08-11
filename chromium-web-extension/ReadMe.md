c449a3f7c0290310a67aaad755ed641c6acd0973
AnjuKrishnaSree-Git <sree.anjukrishna99@gmail.com>
2023-08-12 03:35:58 +0530

    web extension files

---
+++ chromium-web-extension/ReadMe.md
@@ -0,0 +1,13 @@
+# Getting started with To Fake News Detection Chromium Extension
+
+- Go to a random web page
+- Select the text which you want to identify whether its genuine or fake
+- Right Click and Select "Send to fake news Detection Website" from the right click menu
+![Alt text](image.png)
+- Fake News Detection website will be launched with the selected text in the "Content" text box of the website.
+![Alt text](image-1.png)
+
+
+# To Download the extension from Google Chrome Web Store, use the following link
+
+https://chrome.google.com/webstore/detail/to-fake-news-detection/idplbimofaidbkabdmepgmhldmjepajj?hl=en-GB
---
+++ chromium-web-extension/background.js
@@ -0,0 +1,27 @@
+// Create a context menu item
+    chrome.runtime.onInstalled.addListener(function() {
+        chrome.contextMenus.create({
+          id: "sendToApp",
+          title: "Send to Fake News Detection Website",
+          contexts: ["selection"]
+        });
+    });
+
+    // Listen for a context menu click event
+    chrome.contextMenus.onClicked.addListener(function(info, tab) {
+      // Check if the clicked item has a specific ID (you can customize this ID)
+      if (info.menuItemId === "sendToApp") {
+        // Get the selected text from the page
+        sendToApplication(info.selectionText);
+      }
+    });
+
+
+
+
+    // Function to send the highlighted text to the website
+    function sendToApplication(selectedText) {
+        
+        console.log("Selected text:", selectedText);
+        chrome.tabs.create({ url: "http://fakenewsdetect.westeurope.cloudapp.azure.com:3000/?text=" + encodeURIComponent(selectedText) });
+    }
